fd37661cd8b7eff2f17ba218fbd21015088d6d5e
Erik Storrs <estorrs@virtual-workstation1.gsc.wustl.edu>
2019-02-21 12:49:52 -0600

    working test cases

--- mgitools/bsub.py
+++ mgitools/bsub.py
@@ -1,2 +1,56 @@
-def hello_world():
-    print('hello, world!')
+def format_stats(min_memory, max_memory, num_processes):
+    return f"'select[mem>{min_memory * 1000}] rusage[mem={min_memory * 1000}] \
+span[hosts={num_processes}]'"
+
+def generate_bsub_command(command, image, min_memory=1, max_memory=2, num_processes=1,
+        output_file='job.output', log_file='job.log', return_list=False, queue='research-hpc'):
+    """
+    returns a bsub command
+    
+    command
+        - actual command to be run. i.e. python hello_world.py arg1 arg2 ....
+    image
+        - docker image name to use with bsub
+    min_memory
+        - minimum memory for bsub. Takes an int. Units are Gb
+    num_processes
+        - number of process to tell bsub to use
+    return_list
+        - If true, returns a list of commands. Otherwise returns the commands in a string.
+    """
+    args = ['bsub', '-R']
+
+    args += [
+            format_stats(min_memory, max_memory, num_processes),
+            '-M', str(max_memory * 1000000),
+            '-q', queue,
+            '-o', output_file,
+            '-oo', log_file,
+            f"-a 'docker({image})'",
+            f"'{command}'"
+            ]
+
+    if return_list:
+        return args
+    return ' '.join(args)
+
+def generate_bsub_bash_script(commands, image, fp, min_memory=1, max_memory=2, num_processes=1,
+        output_files=None, log_files=None, queue='research-hpc'):
+    """Returns an executable bash script of bsub commands"""
+    if output_files is None:
+        output_files = []
+        for i in range(len(commands)):
+            output_files.append(f'job.{i}.output')
+
+    if log_files is None:
+        log_files = []
+        for i in range(len(commands)):
+            log_files.append(f'job.{i}.log')
+
+    f = open(fp, 'w')
+    for command, output_file, log_file in zip(commands, output_files, log_files):
+        bsub_command = generate_bsub_command(command, image, min_memory=min_memory,
+                max_memory=max_memory, num_processes=num_processes, output_file=output_file,
+                log_file=log_file, return_list=False, queue=queue)
+        f.write(bsub_command + '\n')
+    f.close()
---
+++ tests/test.py
@@ -0,0 +1,73 @@
+import re
+import subprocess
+import time
+
+import pytest
+
+import mgitools.bsub as bsub
+
+DEFAULT_LOG_FP = 'job.log'
+
+
+def test_bsub_hello_world():
+    command = 'echo "hello, world!"'
+
+    bsub_script_fp = 'test.sh'
+    bsub.generate_bsub_bash_script([command], 'python:3.6-jessie', bsub_script_fp)
+
+    result = subprocess.check_output(['bash', bsub_script_fp]).decode('utf-8')
+
+    assert 'is submitted to queue' in result
+
+    job_id = re.sub(r'^.*<([0-9]*)>.*$', r'\1', result).replace('\n', '')
+
+    # wait for setup
+    time.sleep(5)
+
+    while True:
+        bjob_output = subprocess.check_output(['bjobs']).decode('utf-8')
+        if job_id not in bjob_output:
+            break
+        time.sleep(5)
+
+    f = open(DEFAULT_LOG_FP)
+    assert 'Successfully completed' in f.read()
+
+def test_bsub_hello_world_multiple_commands():
+    commands = ['echo "hello, world! 1"',
+            'echo "hello, world! 2"',
+            'echo "hello, world! 3"']
+
+    output_fps = ['test.1.output', 'test.2.output', 'test.3.output']
+    log_fps = ['test.1.log', 'test.2.log', 'test.3.log']
+
+    bsub_script_fp = 'test.sh'
+    bsub.generate_bsub_bash_script(commands, 'python:3.6-jessie', bsub_script_fp,
+            output_files=output_fps, log_files=log_fps)
+
+    result = subprocess.check_output(['bash', bsub_script_fp]).decode('utf-8')
+
+    assert 'is submitted to queue' in result
+
+    # wait for setup
+    time.sleep(1)
+
+    bjob_output = subprocess.check_output(['bjobs']).decode('utf-8')
+    job_ids = []
+    for line in bjob_output.split('\n')[2:]:
+        job_id = re.sub(r'^([0-9]+).*$', r'\1', line).replace('\n', '')
+
+        if job_id.isdigit():
+            job_ids.append(job_id)
+
+    while True:
+        bjob_output = subprocess.check_output(['bjobs']).decode('utf-8')
+        if len([job_id for job_id in job_ids
+            if job_id in bjob_output]) == 0:
+            break
+        time.sleep(5)
+
+    for log_fp in log_fps:
+        f = open(log_fp)
+        assert 'Successfully completed' in f.read()
+        f.close()
